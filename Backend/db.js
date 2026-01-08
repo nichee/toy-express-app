4a894d10fdeb13bae3a52bc5bd9428ea3a882ca4
Nicholas <nichee1308@gmail.com>
2026-01-08 17:10:02 +0800

    Create db queries

--- Backend/db.js
+++ Backend/db.js
@@ -1,15 +1,36 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config();
 
+console.log(process.env.MYSQL_DB)
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'password',
-    database: 'test_schema'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 }).promise()
 
-async function getCompanies(){
+async function getCompanies() {
     const [rows] = await pool.query("select * from companies");
     return rows
 }
 
-const companies = getCompanies();
+async function getCompany(id) {
+    const [row] = await pool.query(`
+    select * 
+    from companies 
+    where id=?
+    `, [id]);
+
+    return row[0]
+}
+
+async function createCompany(company_name, email, password) { // TODO
+    password_hash = bcrypt(password + salt)
+}
+
+const companies = await getCompanies();
+console.log(companies);
+
+const company = await getCompany(1);
+console.log(company);
